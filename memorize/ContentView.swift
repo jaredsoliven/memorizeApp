67b0fec216a6857dd2d09d1b60066df2f878997b
Jared SJ <jaredsj7@gmail.com>
2025-08-04 16:30:26 -0700

    added add/remove card features and scrolling, and more functions

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -7,33 +7,70 @@
 
 /**
  - : view == behaves like a
- - :
+ 
+ // var body == computed property (entire var body variable)
+ // computed == bracketed area, area is computed everytime that area is called
+ // regular property == regular variables
+ // "some" allows anything to be a view as long as it can return as a view
+ // vstack and hstacks
+ // var i: Int // var [name] : type
+ // var s: String
+ // struct: basically a property that can have sub properties, ex) image property with imageScale property
  */
 
 import SwiftUI
 
 struct ContentView: View {
     
-    
-    // var body == computed property (entire var body variable)
-    // computed == bracketed area, area is computed everytime that area is called
-    // regular property == regular variables
-    // "some" allows anything to be a view as long as it can return as a view
-    // vstack and hstacks
-    // var i: Int // var [name] : type
-    // var s: String
-    // struct: basically a property that can have sub properties, ex) image property with imageScale property
+    let emojis = ["👻", "🎃", "🍬", "😈", "💀", "🧙", "😀", "😔", "🏀", "🎾", "🎳", "💧"]
+    @State var cardCount: Int = 4
     
     var body: some View {
-        HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
         }
         .foregroundColor(.orange)
-        .imageScale(.small)
-        .padding()
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemove
+            Spacer()
+            cardAdd
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemove: some View {
+        return cardCountAdjuster(by: -1, symbol : "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdd: some View {
+        return cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
     }
 }
 
@@ -41,19 +78,20 @@ struct ContentView: View {
  Struct that holds properties of the card view
  */
 struct CardView: View {
+    let content: String
     @State var isFaceUp: Bool = false // explicitly defining var type
     var body: some View {
         ZStack {
-            let base = Circle() // let == constant in swift
+            let base = RoundedRectangle(cornerRadius: 12) // let == constant in swift
             // no need to explicitly define type for variables/constants (type inferencing)
             
-            if isFaceUp {
-                base.foregroundColor(.white)
+            Group {
+                base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
-            } else {
-                base.fill()
+                Text(content).font(.largeTitle)
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }
         .onTapGesture { // unable to mutate variables in view, need to change state vars (pointer vars)
             //print("tapped")
