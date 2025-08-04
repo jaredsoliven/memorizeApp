5d639f884a2019a8b56279481b212d8837260afb
Jared SJ <jaredsj7@gmail.com>
2025-08-04 10:38:39 -0700

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Jared SJ on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
